f098f561bf385a9983e61c5b397d3011228327b9
Patrick Stumbaugh <71726173+pstumbaugh@users.noreply.github.com>
2020-10-08 10:41:17 -0700

    Create mainPreludes.cpp

---
+++ Songs List/mainPreludes.cpp
@@ -0,0 +1,77 @@
+
+#include "Preludes.h"
+using namespace std;
+
+//global variables
+const int MAX = 1000;
+
+
+void Preludes::preludesMain()
+{
+    int choice = 1;
+    
+    while (choice)
+    {
+        cout << endl << "PRELUDES MENU" << endl;
+        cout << "Please selection an option below:" << endl;
+        cout << "   1. Display all preludes by title and last changed date." << endl;
+        cout << "   2. View all information for a specific song." << endl;
+        cout << "   3. Edit a specific song." << endl;
+        cout << "   4. Add a new song to Preludes." << endl;
+        cout << "   5. Delete a song from Preludes." << endl;
+        cout << "   0. Save and quit to main menu." << endl;
+        cout << endl;
+        cout << "Your selection: ";
+        cin >> choice;
+        cin.ignore(MAX, '\n');
+        cout << "\n\n";
+        
+        if (choice < 0 || choice > 5) //bad input 
+        {
+            cout << "Invalid input. Please try again" << endl << endl;
+        }
+        
+    //display all titles and last MOD date 
+        else if (choice == 1)
+        {
+            displaySongTitlesAndDateMOD();
+            cout << endl << endl;
+        }
+    //display all information for a specific song
+        else if (choice == 2)
+        {
+            dispalyAllInfoSpecificSong();
+            cout << endl << endl;
+        }
+    //View/edit a song
+        else if (choice == 3)
+        {
+            if (editSong() == false)
+                cout << "ERROR EDITING SONG." << endl << endl;
+        }
+    //Add a new song
+        else if (choice == 4)
+        {
+            if (addNewSong() == false)
+                cout << "ERROR. PLEASE TRY AGAIN." << endl << endl;
+            else
+                cout << endl << "New prelude song added successfully." << endl << endl;
+        }
+    //Delete a song
+        else if (choice == 5)
+        {
+            if (deleteSong() == false)
+                cout << "ERROR. PLEASE TRY AGAIN." << endl << endl;
+            else
+                cout << endl << endl;
+        }
+    //Save and quit to main menu
+        else if (choice == 0)
+        {
+            saveToFile();
+            return;
+        }
+        
+    }
+    return;
+}
