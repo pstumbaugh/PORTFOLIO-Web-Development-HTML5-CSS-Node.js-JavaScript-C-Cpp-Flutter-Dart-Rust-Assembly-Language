c6f310678c8103d096d8dfbb92d26f25fdba3668
Patrick Stumbaugh <71726173+pstumbaugh@users.noreply.github.com>
2020-10-08 10:36:05 -0700

    Create main.h

---
+++ Songs List/main.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "Preludes.h"
+#include "Postludes.h"
+#include "UMHHymns.h"
+#include "FaithWeSing.h"
+
+using namespace std;
